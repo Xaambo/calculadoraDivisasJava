ed90c955689dd038eabad18c2f6f8aa677df2ac3
Jordi Joan <jordi.joan@mataro.epiaedu.cat>
2019-10-14 17:57:36 +0200

    nse la veritat tot borken

--- app/src/main/java/com/example/calculadoradivisasjava/MainActivity.java
+++ app/src/main/java/com/example/calculadoradivisasjava/MainActivity.java
@@ -3,12 +3,147 @@ package com.example.calculadoradivisasjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1 = findViewById(R.id.btn1);
+    Button btn2 = findViewById(R.id.btn2);
+    Button btn3 = findViewById(R.id.btn3);
+    Button btn4 = findViewById(R.id.btn4);
+    Button btn5 = findViewById(R.id.btn5);
+    Button btn6 = findViewById(R.id.btn6);
+    Button btn7 = findViewById(R.id.btn7);
+    Button btn8 = findViewById(R.id.btn8);
+    Button btn9 = findViewById(R.id.btn9);
+    Button btn0 = findViewById(R.id.btn0);
+    Button btnIgual = findViewById(R.id.btnIgual);
+    Button btnCE = findViewById(R.id.btnCE);
+    Button btnDEL = findViewById(R.id.btnDEL);
+    Button btnLibra = findViewById(R.id.btnLibra);
+    Button btnYen = findViewById(R.id.btnYen);
+    Button btnDollar = findViewById(R.id.btnDollar);
+    Button btnYuan = findViewById(R.id.btnYuan);
+    TextView tvIn = findViewById(R.id.tvIn);
+    TextView tvOut = findViewById(R.id.tvOut);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addnumero(1);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addnumero(1);
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addnumero(1);
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addnumero(1);
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addnumero(1);
+            }
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addnumero(1);
+            }
+        });
+
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addnumero(1);
+            }
+        });
+
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addnumero(1);
+            }
+        });
+
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addnumero(1);
+            }
+        });
+
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addnumero(1);
+            }
+        });
+
+        btnCE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnDEL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnIgual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    protected void addnumero(int num) {
+        String actual;
+        int actualInt;
+
+        actual = (String) tvIn.getText();
+
+        actualInt = Integer.parseInt(actual);
+
+        /*if (actualInt == 0) {
+
+        }*/
+
+        actual = String.valueOf(actualInt) + num;
+
+        tvIn.setText(actual);
+    }
+
+    protected void eliminarNumero() {
+
     }
 }
